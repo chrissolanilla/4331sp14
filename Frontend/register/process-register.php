5fe1eded0b4c8f040377e0c555bfd230e2c06133
Daniel Solis <danielasolis.cs@gmail.com>
2023-09-25 21:18:16 +0000

    initial commit of dan branch

---
+++ Frontend/register/process-register.php
@@ -0,0 +1,54 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+var_dump($_POST);
+
+// Start session
+session_start();
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the username, password, and additional user information from the form
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $firstName = $_POST["firstName"];
+    $lastName = $_POST["lastName"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $country = $_POST["country"];
+    $chessRating = $_POST["chessRating"];
+    $favoriteOpening = $_POST["favoriteOpening"];
+    $title = $_POST["title"];
+
+    // Connect to the database
+    $conn = new mysqli("localhost", "newuser", "StrongerPassword123!", "chesscont");
+
+    // Check the connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Hash the password
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    // Prepare a SQL statement to insert the new user
+    $stmt = $conn->prepare("INSERT INTO users (username, password, first_name, last_name, email, phone, country, chess_rating, favorite_opening, title) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssssssss", $username, $hashed_password, $firstName, $lastName, $email, $phone, $country, $chessRating, $favoriteOpening, $title);
+
+    // Execute the statement
+    if ($stmt->execute()) {
+        // Registration successful, redirect to the login page
+        header("Location: ../login/");
+        exit();
+    } else {
+        // Registration failed, redirect back to the registration page with an error
+        echo "Error: " . $stmt->error;
+        exit();
+    }
+
+    // Close the connection
+    $stmt->close();
+    $conn->close();
+}
+?>
