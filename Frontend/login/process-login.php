d155fcf4e45ffc14b6afd1e39c667e3c1043ee75
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-09-29 13:01:11 -0400

    Update process-login.php

--- Frontend/login/process-login.php
+++ Frontend/login/process-login.php
@@ -44,13 +44,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             exit();
         } else {
             // Password is incorrect
-            header("Location: login/login.php");
+            header("Location: login.php");
             echo "Wrong Password";
             exit();
         }
     } else {
         // No user found
-        header("Location: login/login.php");
+        header("Location: login.php");
         echo "Username not found";
         exit();
     }
