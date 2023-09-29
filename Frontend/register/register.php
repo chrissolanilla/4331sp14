737c40e6473acf570778e5c0b0306b3a5161ea0f
Daniel Solis <danielasolis.cs@gmail.com>
2023-09-29 14:37:36 -0400

    attempt 45

--- Frontend/register/register.php
+++ Frontend/register/register.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="register.css">
     <title>Register</title>
 </head>
 <body>
