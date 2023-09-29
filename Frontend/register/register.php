4421cc5f03f0c6becf79534effa42dea0dd0483e
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-09-29 15:53:25 -0400

    Update register.php

--- Frontend/register/register.php
+++ Frontend/register/register.php
@@ -69,7 +69,15 @@
                 
                     
                 echo('Sorry, the Username '.$_POST['username'].' has already been used to register.');
-                    
+                $password = $_POST["password"];
+                $firstName = $_POST["firstName"];
+                $lastName = $_POST["lastName"];
+                $email = $_POST["email"];
+                $phone = $_POST["phone"];
+                $country = $_POST["country"];
+                $chessRating = $_POST["chessRating"];
+                $favoriteOpening = $_POST["favoriteOpening"];
+                $title = $_POST["title"];
 
                     
                 }
