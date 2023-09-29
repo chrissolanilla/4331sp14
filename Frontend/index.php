0a4419f06d14a24c289dc1cf9ff4e16b126b9920
JasperSteensma <jasper@steensma.us>
2023-09-29 14:40:30 -0400

    added background images

--- Frontend/index.php
+++ Frontend/index.php
@@ -8,23 +8,6 @@
      <link rel="stylesheet" href="css/main.css">
      <title>Chess Connect</title>
 
-    <script src="three.r134.min.js"></script>
-    <script src="vanta.dots.min.js"></script>
-    <script>
-    VANTA.DOTS({
-      el: "#vanta",
-      mouseControls: true,
-      touchControls: true,
-      gyroControls: false,
-      minHeight: 200.00,
-      minWidth: 200.00,
-      scale: 1.00,
-      scaleMobile: 1.00,
-      color: 0xcf077,
-      color2: 0x20f1ff,
-      spacing: 37.00
-    })
-  </script>
 
   <style>
     .navbar {
@@ -53,34 +36,14 @@
     <div class="row mt-5">
       <div class="col-md-12">
         <div class="navbar">
-          <a class="navbar-brand" href="#">Chess Connect</a>
+          <a class="navbar-brand" href="#">Welcome to Chess Connect!</a>
+          <p>Find your queen.</p>
           <ul class="navbar-nav ml-auto">
             <!-- Add the login link without custom class -->
             <li class="nav-item">
               <a class="nav-link" href="login/login.php">Login</a>
             </li>
           </ul>
-        </div>
-      </nav>
-    </div>
-    <!--end of nav-->
-    
-    
-	<!-- <img src="https://i.redd.it/j8ldvox5ep891.jpg" > -->
-
-
-  <!-- Main Content -->
-  <div class="vanta" ></div>
-  <div class="container">
-    <div class="row mt-5">
-      <div class="col-md-12">
-        <h1>Welcome to Chess Connect!</h1>
-        <p>Find your queen.</p>
-        <div class="chess-piece chess-piece1">
-          <img src="https://pics.clipartpng.com/Pawn_White_Chess_Piece_PNG_Clip_Art-2751.png" class="image-pawn" />
-        </div>
-        <div class="chess-piece chess-piece2">
-          <img src="https://pics.clipartpng.com/Pawn_White_Chess_Piece_PNG_Clip_Art-2751.png" class="image-pawn" />
         </div>
       </div>
     </div>
--- Frontend/login/login.php
+++ Frontend/login/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="../css/main.css">
     <title>Login - Chess Contact Manager</title>
     <style>
         /* Custom CSS to vertically center the login container */
@@ -33,23 +33,6 @@
 </head>
 <body class="background-image2"> <!-- Apply background class to the body -->   
     <div class="container vertical-center"> <!-- Add the vertical-center class to center vertically -->
-</head>
-<body>
-            <!-- Navigation Bar should be placed at the top so have it be the first thing in the body -->
-            <div class="container">
-            <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
-                <a class="navbar-brand" href="../#">Chess Connect</a>
-                <div class="navbar">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                    <a class="nav-link" >Login</a>
-                    </li>
-                </ul>
-                </div>
-            </nav>
-        </div>
-        <!--end of nav-->
-    <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="card">
