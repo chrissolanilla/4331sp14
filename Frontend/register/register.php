9e362a370fcc05915982fa48886840d8f09a539a
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-09-29 20:30:19 -0400

    Update register.php

--- Frontend/register/register.php
+++ Frontend/register/register.php
@@ -14,7 +14,7 @@
                 <div class="navbar">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                    <a class="nav-link" href="login/login.php">Login</a>
+                    <a class="nav-link" href="https://chessconnect.xyz/login/login.php">Login</a>
                     </li>
                 </ul>
                 </div>
