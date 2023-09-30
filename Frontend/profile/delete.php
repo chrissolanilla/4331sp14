92eb84f954ae445eed7aa8a7a6b7f18a4d814d23
Jack6131 <44846289+Jack6131@users.noreply.github.com>
2023-09-30 14:12:07 -0400

    Update delete.php

--- Frontend/profile/delete.php
+++ Frontend/profile/delete.php
@@ -1,1 +1,27 @@
+<?php
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+// Get the username, password, and additional user information from the form
+    
+
+// Connect to the database
+    $conn = new mysqli("localhost", "newuser", "StrongerPassword123!", "chesscont");
+
+    // Check the connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Prepare a SQL statement to insert the new user
+    $delete=mysqli($conn,"DELETE FROM contacts WHERE $_GET['id']");
+    
+
+    // Execute the statement
+   
+
+    // Close the connection
+    $stmt->close();
+    $conn->close();
+    }
+?>
